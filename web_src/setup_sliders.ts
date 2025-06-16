c72fe8a0ee04fbc4cd81f51082535546de04b4c7
DiscRoomFan36 <fletcherhogarth@gmail.com>
2025-06-16 20:16:53 +1000

    break the slider function out into seperate functions.

--- web_src/setup_sliders.ts
+++ web_src/setup_sliders.ts
@@ -102,95 +102,133 @@ export function setup_sliders(properties: [string, string][], set_property: (nam
         // TODO some way to print an object.
         // log(Log_Type.Debug_Sliders, `property struct ${property_struct}`);
 
-        const id = `slider_${name}`;
-        const para_id = `${id}_paragraph`;
-        const paragraph_text = `${name.replace(/_/g, " ")}`
-
-        var initial_value;
-        var initial_value_for_text;
-
         switch (property_struct.property_type) {
         case Property_Type.Property_Float:
-            initial_value = property_struct.float_default;
-            initial_value_for_text = initial_value.toPrecision(2)
+            make_float_slider(slider_container, name, property_struct, set_property)
             break;
+
         case Property_Type.Property_Int:
-            initial_value = property_struct.int_default;
-            initial_value_for_text = initial_value
+            make_int_slider(slider_container, name, property_struct, set_property)
             break;
 
-        default: throw new Error(`Unknown type in 'initial value' ${name}`);
+        default: throw new Error(`Unknown property type ${property_struct.property_type}`);
         }
-
-        const map_range_to_slider_number = (x: number): number => {
-            switch (property_struct.property_type) {
-            case Property_Type.Property_Float: {
-                const min = property_struct.float_range_min;
-                const max = property_struct.float_range_max;
-                return (x-min)/(max-min)*(1000-0) + 0
-            }
-
-            case Property_Type.Property_Int: {
-                const min = property_struct.int_range_min;
-                const max = property_struct.int_range_max;
-                return (x-min)/(max-min)*(1000-0) + 0
-            }
-
-            default: throw new Error(`Unknown type in 'map_range_to_slider_number' ${name}`);
-            }
-
-        }
-        const map_range_to_real_range = (x: number): number => {
-            switch (property_struct.property_type) {
-            case Property_Type.Property_Float: {
-                const min = property_struct.float_range_min;
-                const max = property_struct.float_range_max;
-                // TODO toPrecision might not be the best function for formatting. margin is being messed with (1.0e+2)
-                // make it the proper value // TODO is this correct? even for small values?
-                return parseFloat(((x-0)/(1000-0)*(max-min) + min).toPrecision(2))
-            }
-
-            case Property_Type.Property_Int: {
-                const min = property_struct.int_range_min;
-                const max = property_struct.int_range_max;
-                // make it the proper value
-                return Math.floor((x-0)/(1000-0)*(max-min) + min)
-            }
-
-            default: throw new Error(`Unknown type in 'map_range_to_real_range' ${name}`);
-            }
-        }
-
-        // TODO a lot of numbers must be between 0-1, because sliders only use ints (look up if this is the case.) we will have to get creative
-        // TODO use step. might clean this up a bit.
-        const html_string = `
-            <p class="sliderKey" id="${para_id}">
-                ${paragraph_text}: ${initial_value_for_text}
-            </p>
-            <input type="range" min="0" max="1000" value="${map_range_to_slider_number(initial_value)}" class="slider" id="${id}">
-            `;
-
-        const new_thing = document.createElement("div");
-        new_thing.className = "rangeHolder";
-        new_thing.innerHTML = html_string;
-
-        slider_container.appendChild(new_thing);
-
-        const slider = document.getElementById(id) as HTMLInputElement | null;
-        if (slider === null) throw new Error("Could not find the slider");
-
-
-        slider.addEventListener("input", (event) => {
-            const slider_value_string = (event.target as HTMLInputElement).value;
-
-            const slider_number = map_range_to_real_range(Number(slider_value_string));
-
-            const slider_text = document.getElementById(para_id) as HTMLParagraphElement | null;
-            if (slider_text === null) throw new Error(`could not find slider_text ${para_id}`);
-
-            slider_text.textContent = `${paragraph_text}: ${slider_number}`
-
-            set_property(name, slider_number)
-        })
     }
+}
+
+
+///////////////////////////////////////////////
+//         Make a slider for an float
+///////////////////////////////////////////////
+
+function make_float_slider(slider_container: HTMLElement, name: string, property_struct: Property_Struct, set_property: (name:string, value:number) => void) {
+    const id = `slider_${name}`;
+    const para_id = `${id}_paragraph`;
+    const paragraph_text = `${name.replace(/_/g, " ")}`
+
+    const initial_value = property_struct.float_default;
+    const initial_value_for_text = initial_value.toPrecision(2);
+
+    const map_range_to_slider_number = (x: number): number => {
+        const min = property_struct.float_range_min;
+        const max = property_struct.float_range_max;
+        return (x-min)/(max-min)*(1000-0) + 0
+    }
+    const map_range_to_real_range = (x: number): number => {
+        const min = property_struct.float_range_min;
+        const max = property_struct.float_range_max;
+        // TODO toPrecision might not be the best function for formatting. margin is being messed with (1.0e+2)
+        // make it the proper value // TODO is this correct? even for small values?
+        return parseFloat(((x-0)/(1000-0)*(max-min) + min).toPrecision(2))
+    }
+
+    // TODO a lot of numbers must be between 0-1, because sliders only use ints (look up if this is the case.) we will have to get creative
+    // TODO use step. might clean this up a bit.
+    const html_string = `
+        <p class="sliderKey" id="${para_id}">
+            ${paragraph_text}: ${initial_value_for_text}
+        </p>
+        <input type="range" min="0" max="1000" value="${map_range_to_slider_number(initial_value)}" class="slider" id="${id}">
+        `;
+
+    const new_thing = document.createElement("div");
+    new_thing.className = "rangeHolder";
+    new_thing.innerHTML = html_string;
+
+    slider_container.appendChild(new_thing);
+
+    const slider = document.getElementById(id) as HTMLInputElement | null;
+    if (slider === null) throw new Error("Could not find the slider");
+
+
+    slider.addEventListener("input", (event) => {
+        const slider_value_string = (event.target as HTMLInputElement).value;
+
+        const slider_number = map_range_to_real_range(Number(slider_value_string));
+
+        const slider_text = document.getElementById(para_id) as HTMLParagraphElement | null;
+        if (slider_text === null) throw new Error(`could not find slider_text ${para_id}`);
+
+        slider_text.textContent = `${paragraph_text}: ${slider_number}`
+
+        set_property(name, slider_number)
+    })
+}
+
+
+///////////////////////////////////////////////
+//          Make a slider for an int
+///////////////////////////////////////////////
+
+function make_int_slider(slider_container: HTMLElement, name: string, property_struct: Property_Struct, set_property: (name:string, value:number) => void) {
+    const id = `slider_${name}`;
+    const para_id = `${id}_paragraph`;
+    const paragraph_text = `${name.replace(/_/g, " ")}`
+
+    const initial_value = property_struct.int_default;
+    const initial_value_for_text = initial_value;
+
+    const map_range_to_slider_number = (x: number): number => {
+        const min = property_struct.int_range_min;
+        const max = property_struct.int_range_max;
+        return (x-min)/(max-min)*(1000-0) + 0
+    }
+    const map_range_to_real_range = (x: number): number => {
+        const min = property_struct.int_range_min;
+        const max = property_struct.int_range_max;
+        // make it the proper value
+        return Math.floor((x-0)/(1000-0)*(max-min) + min)
+    }
+
+    // TODO a lot of numbers must be between 0-1, because sliders only use ints (look up if this is the case.) we will have to get creative
+    // TODO use step. might clean this up a bit.
+    const html_string = `
+        <p class="sliderKey" id="${para_id}">
+            ${paragraph_text}: ${initial_value_for_text}
+        </p>
+        <input type="range" min="0" max="1000" value="${map_range_to_slider_number(initial_value)}" class="slider" id="${id}">
+        `;
+
+    const new_thing = document.createElement("div");
+    new_thing.className = "rangeHolder";
+    new_thing.innerHTML = html_string;
+
+    slider_container.appendChild(new_thing);
+
+    const slider = document.getElementById(id) as HTMLInputElement | null;
+    if (slider === null) throw new Error("Could not find the slider");
+
+
+    slider.addEventListener("input", (event) => {
+        const slider_value_string = (event.target as HTMLInputElement).value;
+
+        const slider_number = map_range_to_real_range(Number(slider_value_string));
+
+        const slider_text = document.getElementById(para_id) as HTMLParagraphElement | null;
+        if (slider_text === null) throw new Error(`could not find slider_text ${para_id}`);
+
+        slider_text.textContent = `${paragraph_text}: ${slider_number}`
+
+        set_property(name, slider_number)
+    })
 }
